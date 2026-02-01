89ef100cd29c822c6631b4ebd26298fd081d97e4
KikolasDEV <kikoamarillo94@gmail.com>
2026-02-01 23:59:16 +0100

    JS Basics: Booleans

---
+++ JS Basics/Booleans/challenge01.js
@@ -0,0 +1,11 @@
+/*
+    Assign boolean to a variable
+    Here, we declare the variable isTrue. But, it has no value yet. Assign a boolean value to it and run the code.
+    
+    let isTrue;
+    console.log(isTrue);
+*/
+
+let isTrue;
+isTrue = true;
+console.log(isTrue);
