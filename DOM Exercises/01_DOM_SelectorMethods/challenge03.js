a14feea6a3206a3911e4c707d6ab47becc75461f
KikolasDEV <kikoamarillo94@gmail.com>
2026-02-10 14:24:38 +0100

    DOM Exercises: DOM Selector Methods

---
+++ DOM Exercises/01_DOM_SelectorMethods/challenge03.js
@@ -0,0 +1,27 @@
+/*
+    Select multiple HTML elements
+    In this scenario, we are looking for a list of elements gathered in one variable - rather than only one element. 
+    Assign the list items in the view to the variable 'listItems' by using an appropriate selector method.
+    Once you have completed the code below, verify it by hovering over the list items until all items have the value 'ON'
+
+    // assign the correct elements to the variable
+    const listItems = 
+
+    const handleHover = (event) => {
+      return event.target.innerText = 'ON';
+    };
+
+    if(listItems.length > 1) {
+      listItems.forEach(item => item.addEventListener('mouseover', handleHover));
+    }
+*/
+
+const listItems = document.querySelectorAll("#list li");
+
+const handleHover = (event) => {
+  return event.target.innerText = 'ON';
+};
+
+if(listItems.length > 1) {
+  listItems.forEach(item => item.addEventListener('mouseover', handleHover));
+}
