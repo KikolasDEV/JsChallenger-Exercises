47239bf090400975a11a668bf6ee8c1a6fd53b7f
KikolasDEV <kikoamarillo94@gmail.com>
2026-02-13 20:22:33 +0100

    Fundamentals.1

---
+++ JavaScript Practice/01_JavaScript_Fundamentals/challenge01.js
@@ -0,0 +1,26 @@
+/*
+    Sum two numbers
+    Write a function that takes two numbers (a and b) as argument. Sum a and b. Return the result
+
+    function myFunction(a, b) {
+        return 
+    }
+ 
+    console.log(myFunction(1, 2))
+    // expected: 3 
+    console.log(myFunction(1, 10))
+    // expected: 11 
+    console.log(myFunction(99, 1))
+    // expected: 100 
+*/
+
+function myFunction(a, b) {
+   return a + b;
+}
+ 
+console.log(myFunction(1, 2))
+// expected: 3 
+console.log(myFunction(1, 10))
+// expected: 11 
+console.log(myFunction(99, 1))
+// expected: 100 
