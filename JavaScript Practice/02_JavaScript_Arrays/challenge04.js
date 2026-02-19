8526558ec9461ea62f289fd29c19724623b5c644
KikolasDEV <kikoamarillo94@gmail.com>
2026-02-19 13:10:15 +0100

    Array: Slice()

---
+++ JavaScript Practice/02_JavaScript_Arrays/challenge04.js
@@ -0,0 +1,26 @@
+/*
+    Get first n elements of an array
+    Write a function that takes an array (a) as argument. Extract the first 3 elements of a. Return the resulting array
+
+    function myFunction(a) {
+       return 
+    }
+    
+    console.log(myFunction([1,2,3,4]))
+    // expected: [1,2,3] 
+    console.log(myFunction([5,4,3,2,1,0]))
+    // expected: [5,4,3] 
+    console.log(myFunction([99,1,1]))
+    // expected: [99,1,1] 
+*/
+
+function myFunction(a) {
+   return a.slice(0, 3); 
+}
+ 
+console.log(myFunction([1,2,3,4]))
+// expected: [1,2,3] 
+console.log(myFunction([5,4,3,2,1,0]))
+// expected: [5,4,3] 
+console.log(myFunction([99,1,1]))
+// expected: [99,1,1] 
---
+++ JavaScript Practice/02_JavaScript_Arrays/challenge05.js
@@ -0,0 +1,26 @@
+/*
+    Return last n array elements
+    Write a function that takes an array (a) and a number (n) as arguments. It should return the last n elements of a.
+
+    function myFunction(a, n) {
+       return 
+    }
+    
+    console.log(myFunction([1, 2, 3, 4, 5], 2))
+    // expected: [ 4, 5 ] 
+    console.log(myFunction([1, 2, 3], 6))
+    // expected: [ 1, 2, 3 ] 
+    console.log(myFunction([1, 2, 3, 4, 5, 6, 7, 8], 3))
+    // expected: [ 6, 7, 8 ] 
+*/
+
+function myFunction(a, n) {
+   return a.slice(-n);
+}
+ 
+console.log(myFunction([1, 2, 3, 4, 5], 2))
+// expected: [ 4, 5 ] 
+console.log(myFunction([1, 2, 3], 6))
+// expected: [ 1, 2, 3 ] 
+console.log(myFunction([1, 2, 3, 4, 5, 6, 7, 8], 3))
+// expected: [ 6, 7, 8 ] 
