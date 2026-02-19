7d4828a58f5f38afae9a97068e6d71e8be47bdf5
KikolasDEV <kikoamarillo94@gmail.com>
2026-02-19 12:11:16 +0100

    Fundamentals.6.4

---
+++ JavaScript Practice/01_JavaScript_Fundamentals/challenge24.js
@@ -0,0 +1,29 @@
+/*
+    Find the correct word by incrementing letters in alphabet
+    Write a function that takes a string as argument. As it is, the string has no meaning. 
+    Increment each letter to the next letter in the alphabet. Return the correct word
+
+    function myFunction(str) {
+    
+    }
+    
+    console.log(myFunction('bnchmf'))
+    // expected: 'coding' 
+    console.log(myFunction('bgddrd'))
+    // expected: 'cheese' 
+    console.log(myFunction('sdrshmf'))
+    // expected: 'testing' 
+*/
+
+function myFunction(str) {
+	const arr = [...str];
+    const correctedArray = arr.map(e => String.fromCharCode(e.charCodeAt()+1));
+    return correctedArray.join('');
+}
+ 
+console.log(myFunction('bnchmf'))
+// expected: 'coding' 
+console.log(myFunction('bgddrd'))
+// expected: 'cheese' 
+console.log(myFunction('sdrshmf'))
+// expected: 'testing' 
