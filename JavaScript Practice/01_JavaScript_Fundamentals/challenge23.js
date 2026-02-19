464bb6b468ce98c1b93d2550fecf4ce0b68083dd
KikolasDEV <kikoamarillo94@gmail.com>
2026-02-19 12:11:10 +0100

    Fundamentals.6.3

---
+++ JavaScript Practice/01_JavaScript_Fundamentals/challenge23.js
@@ -0,0 +1,34 @@
+/*
+    Insert character after every n characters (backwards)
+    Write a function that takes two strings (a and b) as arguments. 
+    Beginning at the end of 'a', insert 'b' after every 3rd character of 'a'. 
+    Return the resulting string.
+
+    function myFunction(a, b) {
+    
+    }
+    
+    console.log(myFunction('1234567','.'))
+    // expected: '1.234.567' 
+    console.log(myFunction('abcde','$'))
+    // expected: 'ab$cde' 
+    console.log(myFunction('zxyzxyzxyzxyzxyz','w'))
+    // expected: 'zwxyzwxyzwxyzwxyzwxyz' 
+*/
+
+function myFunction(a, b) {
+	let result = [];
+    let rest = a;
+    while (rest.length) {
+   	    result.push(rest.slice(-3));
+   	    rest = rest.slice(0, -3);
+    }
+    return result.reverse().join(b);
+}
+ 
+console.log(myFunction('1234567','.'))
+// expected: '1.234.567' 
+console.log(myFunction('abcde','$'))
+// expected: 'ab$cde' 
+console.log(myFunction('zxyzxyzxyzxyzxyz','w'))
+// expected: 'zwxyzwxyzwxyzwxyzwxyz' 
