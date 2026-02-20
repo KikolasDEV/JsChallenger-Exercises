c680e39d4468eb3c77dbbdc7b057c7a5fe128869
KikolasDEV <kikoamarillo94@gmail.com>
2026-02-20 20:26:46 +0100

    Arrays: Advanced II (Pendiente)

---
+++ JavaScript Practice/02_JavaScript_Arrays/challenge22.js
@@ -0,0 +1,26 @@
+/*
+    Get every nth element of array
+    Write a function that takes an array (a) and a value (n) as arguments. Save every nth element in a new array. Return the new array
+
+    function myFunction(a, n) {
+    
+    }
+    
+    console.log(myFunction([1,2,3,4,5,6,7,8,9,10], 3))
+    // expected: [3,6,9] 
+    console.log(myFunction([10,9,8,7,6,5,4,3,2,1], 5))
+    // expected: [6,1] 
+    console.log(myFunction([7,2,1,6,3,4,5,8,9,10], 2))
+    // expected: [2,6,4,8,10] 
+*/
+
+function myFunction(a, n) {
+	
+}
+ 
+console.log(myFunction([1,2,3,4,5,6,7,8,9,10], 3))
+// expected: [3,6,9] 
+console.log(myFunction([10,9,8,7,6,5,4,3,2,1], 5))
+// expected: [6,1] 
+console.log(myFunction([7,2,1,6,3,4,5,8,9,10], 2))
+// expected: [2,6,4,8,10] 
